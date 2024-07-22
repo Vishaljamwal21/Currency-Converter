7f285cea2472e8fc2028d3dd2dea3dca2faf10e0
vishaljamwal <vishaljamwal402@gmail.com>
2024-07-22 12:35:59 +0530

    Register-Login

--- CurrencyConverter/CurrencyConverter/Data/Migrations/20240722053707_initLoad.Designer.cs
+++ CurrencyConverter/CurrencyConverter/Data/Migrations/20240722053707_initLoad.Designer.cs
@@ -4,6 +4,7 @@ using CurrencyConverter.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CurrencyConverter.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240722053707_initLoad")]
+    partial class initLoad
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
